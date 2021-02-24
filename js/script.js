4591140c14866795b0ae46b1306d7ca362ade15d
Taras <tarikmanos@gmail.com>
2021-02-24 11:14:05 +0200

    first programm

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log(123);
+"use strict";
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ''),
+    personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+    };
+
+for (let i = 0; i<2; i++) {
+    personalMovieDB.movies[prompt('Один из последних просмотренных фильмов?','')] = prompt('На сколько оцените его?','');
+}
+
+console.log(personalMovieDB);
